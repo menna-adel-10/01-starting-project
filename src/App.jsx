8053cb13a5e3e2ca921a64840eaa04a0c4dfbb6c
menna-adel-10 <menna10_adel@hotmail.com>
2024-03-18 00:12:16 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <Sidebar />
       <NewProject />
     </main>
