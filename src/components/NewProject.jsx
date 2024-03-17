94018b399753fa5173e6a928951460198397f690
menna-adel-10 <menna10_adel@hotmail.com>
2024-03-18 00:58:18 +0200

    update

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react'
 import Input from './Input'
 
-const NewProject = () => {
+const NewProject = ({onAdd}) => {
     let title = useRef()
     let description = useRef()
     let date = useRef()
@@ -10,6 +10,12 @@ const NewProject = () => {
         const enteredTitle = title.current.value;
         const enteredDescription = description.current.value;
         const enteredDate = date.current.value;
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            date: enteredDate
+        });
     }
 
     return (
@@ -19,9 +25,9 @@ const NewProject = () => {
               <li><button onClick={handleSave} className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
           </menu>
             <div>
-              <Input ref={title} label="Title"/>
+              <Input type="text" ref={title} label="Title"/>
               <Input ref={description} label="Description" textarea/>
-              <Input ref={date} label="Due Date"/>
+              <Input type="date" ref={date} label="Due Date"/>
           </div>
             
       </div>
