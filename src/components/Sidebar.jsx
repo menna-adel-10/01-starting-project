288e2fd1012e6df0866fa629172953003e581e9e
menna-adel-10 <menna10_adel@hotmail.com>
2024-03-17 23:43:57 +0200

    .

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Sidebar = () => {
   return (
       <aside>
-          
+          <h2>Your Projects</h2>
+          <div>
+              <button>
+                  Add Project
+              </button>
+          </div>
       </aside>
   )
 }
