02af5be17d8999ac11461507785378ee426bb7cd
menna-adel-10 <menna10_adel@hotmail.com>
2024-03-19 22:38:52 +0200

    .

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,10 +1,12 @@
 import React, { useRef } from 'react'
 import Input from './Input'
+import Modal from './Modal'
 
-const NewProject = ({onAdd}) => {
-    let title = useRef()
-    let description = useRef()
-    let date = useRef()
+const NewProject = ({ onAdd }) => {
+    const modal = useRef()
+    const title = useRef()
+    const description = useRef()
+    const date = useRef()
 
     function handleSave() {
         const enteredTitle = title.current.value;
@@ -15,7 +17,8 @@ const NewProject = ({onAdd}) => {
             enteredDescription.trim() === '' ||
             enteredDate.trim() === ''
         ) {
-            
+            modal.current.open();
+            return;
         }
 
         onAdd({
@@ -26,7 +29,13 @@ const NewProject = ({onAdd}) => {
     }
 
     return (
-      <div className="w-[35rem] mt-16">
+        <>
+            <Modal ref={modal} buttonCaption="Ok">
+                <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+                <p className="text-stone-600 mb-4">Oops ... looks like you forgot to enter a value</p>
+                <p className="text-stone-600 mb-4">Please make sure you provide a valid value for every input field.</p>
+            </Modal>
+         <div className="w-[35rem] mt-16">
           <menu className="flex items-center justify-end gap-4 my-4">
               <li><button className="text-stone-600 duration-300 hover:text-stone-950">Cancel</button></li>
               <li><button onClick={handleSave} className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
@@ -38,6 +47,7 @@ const NewProject = ({onAdd}) => {
           </div>
             
       </div>
+        </>
   ) 
 }
 
